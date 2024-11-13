361972b95c07ca28ab056ab3e1250b67ba1b7ba6
carsiro96it <carsiro96.ittools@hotmail.com>
2024-11-13 02:14:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ s_target_url = "https://www.bing.com/search?pglt=43&q=qu%C3%A9+han+aprobado+en+e
 
 s_UserAgent = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36","Accept-Language":"en-US,en;q=0.9","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","Accept-Encoding":"gzip, deflate, br","upgrade-insecure-requests":"1"}
 # ➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
-resp = requests.get(   s_target_url,
-                    headers = s_UserAgent   )
+resp = requests.get(   s_target_url
+                    # , headers = s_UserAgent   
+                   )
 # print(resp.text[0:10000])
 # resp.encoding = resp.apparent_encoding
 # print(resp.text[0:10000])
