2b9291016b06b9b835091c16a638b5180b98bcaf
carsiro96it <carsiro96.ittools@hotmail.com>
2024-11-13 01:36:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,14 @@ s_UserAgent = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebK
 # ➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
 resp = requests.get(   s_target_url,
                     headers = s_UserAgent   )
+if resp.status_code == 200:
+    content_length = resp.headers.get('Content-Length')
+    if content_length and len(resp.content) != int(content_length):
+        print("⚠️⚠️ Advertencia: El contenido recibido no coincide con el tamaño esperado.")
+    else:
+        print("✅✅ El contenido parece estar completo.")
+else:
+    print(f"❌❌ Error al obtener la respuesta: {resp.status_code}")
 content_encoding = resp.headers.get('Content-Encoding')
 print(f"Content-Encoding: {content_encoding}")
 print(f"URL final: {resp.url}")
