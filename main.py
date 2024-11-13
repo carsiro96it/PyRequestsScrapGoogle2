f46f8348981494844ed4a67d87710a3a723e5afa
carsiro96it <carsiro96.ittools@hotmail.com>
2024-11-13 01:46:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import brotli, inspect, logging, platform , requests, sys
 from datetime import datetime
+import pandas as pd
 
 s_system = platform.system()
 if s_system == "Linux":
@@ -30,6 +31,7 @@ s_UserAgent = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebK
 # ➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
 resp = requests.get(   s_target_url,
                     headers = s_UserAgent   )
+print(    pd.read_html(resp.text, attrs={'width': '656'}, header=0, parse_dates=['Advertised Date']).head()    )
 if resp.status_code == 200:
     content_length = resp.headers.get('Content-Length')
     if content_length and len(resp.content) != int(content_length):
@@ -45,15 +47,16 @@ print(f"Redirigido: {resp.history}")
 # content_type = resp.headers.get('Content-Type')
 # print(f"Content-Type: {content_type}")
 
+
 # Descomprimir si está codificado con Brotli
-if content_encoding == 'br':
+# if content_encoding == 'br':
     # Descomprimir usando Brotli
-    decompressed_content = brotli.decompress(resp.content)
-    # Decodificar el contenido descomprimido como UTF-8
-    resp = decompressed_content.decode('utf-8', errors='replace')
-else:
+    # decompressed_content = brotli.decompress(resp.content)
+    # # Decodificar el contenido descomprimido como UTF-8
+#     resp = decompressed_content.decode('utf-8', errors='replace')
+# else:
     # Si no está comprimido, decodificar directamente
-    resp = resp.content.decode('utf-8', errors='replace')
+    # resp = resp.content.decode('utf-8', errors='replace')
 # Verificar el tipo de contenido
 
 print(  resp  )
